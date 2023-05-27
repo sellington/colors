e1a24e290da0531504154a0212cf5b52f3322241
sellington <sellington011@gmail.com>
2023-05-27 16:25:35 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const ip = require('ip');
+const fs = require('fs');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    fs.appendFile('ips.txt', `${clientIp}\n`, function (err) {
+        if (err) throw err;
+    });
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
