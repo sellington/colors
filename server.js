abfa122d012936c88b708cf65cc80551aaeea4b1
sellington <sellington011@gmail.com>
2023-05-27 22:47:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(443, () => console.log('Server is running on port 3000'));
+app.listen(443, () => console.log('Server is running on port 443'));
